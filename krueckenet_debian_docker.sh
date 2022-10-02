4b1e3179949a13df5d53dafa26ed37cdce38fc44
diekruecke <44458271+diekruecke@users.noreply.github.com>
2022-10-03 00:12:49 +0200

    Create krueckenet_debian_docker.sh

---
+++ krueckenet_debian_docker.sh
@@ -0,0 +1,28 @@
+function msg_info() {
+    local msg="$1"
+    echo -ne "${msg}"
+}
+
+msg_info "Beginne Docker installation"
+
+apt-get update
+apt-get install ca-certificates curl gnupg lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+groupadd docker
+usermod -aG docker $USER
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+msg_info "scheint geklappt zu haben"
+msg_info echo docker version
