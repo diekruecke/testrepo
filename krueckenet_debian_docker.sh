9a5c6112943e0a5a4a08e7bb871450f08b9123e8
diekruecke <44458271+diekruecke@users.noreply.github.com>
2022-10-03 01:49:34 +0200

    Update krueckenet_debian_docker.sh

--- krueckenet_debian_docker.sh
+++ krueckenet_debian_docker.sh
@@ -27,4 +27,4 @@ docker volume create portainer_data &>/dev/null
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest  &>/dev/null
 
 msg_info "scheint geklappt zu haben"
-msg_info "$(echo $docker version)"
+msg_info "$(echo $(docker version))"
