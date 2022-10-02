8c65bf0fb3a2ec4fa20e93dc47a1116c7a7dfc06
diekruecke <44458271+diekruecke@users.noreply.github.com>
2022-10-02 23:21:10 +0200

    Create krueckenet_debian_install.sh

---
+++ krueckenet_debian_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+locale-gen >/dev/null
+
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+
+apt-get install -y curl &>/dev/null
+apt-get install -y sudo &>/dev/null
+
+PASS=$(grep -w "root" /etc/shadow | cut -b6);
+  if [[ $PASS != $ ]]; then
+chmod -x /etc/update-motd.d/*
+touch ~/.hushlogin
+GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
+mkdir -p $(dirname $GETTY_OVERRIDE)
+cat << EOF > $GETTY_OVERRIDE
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+EOF
+systemctl daemon-reload
+systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+  fi
+
+apt-get autoremove >/dev/null
+apt-get autoclean >/dev/null
